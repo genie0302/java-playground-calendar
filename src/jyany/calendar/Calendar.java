6adad8d98ff324fe31f3ca8fa75eb3ab8af3b75a
jyanii268 <jyanii268@gmail.com>
2023-09-30 15:10:03 +0900

    Implement methods of Calendar class.

--- src/jyany/calendar/Calendar.java
+++ src/jyany/calendar/Calendar.java
@@ -3,18 +3,25 @@ package jyany.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-    public static void main(String[] args) {
+    private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public void printSampleCalendar() {
         System.out.println("일 월  화 수  목 금  토");
         System.out.println("--------------------");
         System.out.println(" 1  2  3  4  5  6  7");
         System.out.println(" 8  9 10 11 12 13 14");
         System.out.println("15 16 17 18 19 20 21");
         System.out.println("22 23 24 25 26 27 28");
+    }
+    public int getMaxDaysOfMonth (int month){
+        return MAX_DAYS[month];
+    }
+    public static void main(String[] args) {
 
-        int[] daysOfMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         System.out.println("달을 입력하세요.");
         Scanner sc = new Scanner(System.in);
         int month = sc.nextInt();
-        System.out.printf("%d월은 %d일까지 있습니다.\n", month, daysOfMonth[month]);
+        Calendar cal = new Calendar();
+        System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+        cal.printSampleCalendar();
     }
 }
