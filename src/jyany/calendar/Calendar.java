3aa11ec51b4837695ee7072d7f4c2e6c9b965c66
jyanii268 <jyanii268@gmail.com>
2023-10-03 15:42:11 +0900

    Implement print calendar based on user's start day input

--- src/jyany/calendar/Calendar.java
+++ src/jyany/calendar/Calendar.java
@@ -3,6 +3,24 @@ package jyany.calendar;
 public class Calendar {
     private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     private static final int[] LEAP_YEAR_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public void printCalendar(int year, int month, String weekday) {
+        System.out.printf("   <<%4d년%3d월>>\n", year, month);
+        System.out.println("SU MO TU WE TH FR SA");
+        System.out.println("--------------------");
+
+        int maxDays = getMaxDaysOfMonth(year, month);
+        int spaceSize = parseDay(weekday);
+        for (int i = 0; i < spaceSize; i++){
+            System.out.print("   ");
+        }
+        for (int i = 1; i <= maxDays; i++){
+            System.out.printf("%2d ", i);
+            if ((spaceSize + i) % 7 == 0){
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
     public boolean isLeapYear(int year) {
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
             return true;
@@ -11,34 +29,35 @@ public class Calendar {
             return false;
         }
     }
-    public void printCalendar(int year, int month) {
-        System.out.printf("   <<%4d년%3d월>>\n", year, month);
-        System.out.println("Su Mo Tu We Th Fr Sa");
-        System.out.println("--------------------");
-
-        int maxDays = getMaxDaysOfMonth(year, month);
-        for (int i = 1; i <= maxDays; i++){
-            System.out.printf("%2d ", i);
-            if (i % 7 == 0){
-                System.out.println();
-            }
-        }
-        System.out.println();
-
-//        System.out.println("Su Mo Tu We Th Fr Sa");
-//        System.out.println("--------------------");
-//        System.out.println(" 1  2  3  4  5  6  7");
-//        System.out.println(" 8  9 10 11 12 13 14");
-//        System.out.println("15 16 17 18 19 20 21");
-//        System.out.println("22 23 24 25 26 27 28");
-//        System.out.println("29 30 31");
-    }
     public int getMaxDaysOfMonth (int year, int month){
         if (isLeapYear(year)){
             return LEAP_YEAR_MAX_DAYS[month];
         }
         else {
             return MAX_DAYS[month];
+        }
+    }
+
+    /*
+    * @param weekday 요일명
+    * @return 0~6 (0 = Sunday, 6 = Saturday)
+    */
+    public int parseDay (String weekday) {
+        switch (weekday){
+            case "MO":
+                return 1;
+            case "TU":
+                return 2;
+            case "WE":
+                return 3;
+            case "TH":
+                return 4;
+            case "FR":
+                return 5;
+            case "SA":
+                return 6;
+            default:
+                return 0;
         }
     }
 }
--- src/jyany/calendar/Prompt.java
+++ src/jyany/calendar/Prompt.java
@@ -9,10 +9,9 @@ public class Prompt {
         Calendar cal = new Calendar();
 
         while (true){
-            System.out.println("년도를 입력하세요.");
+            System.out.println("년도를 입력하세요.(exit: -1)");
             System.out.print("YEAR> ");
             int year = sc.nextInt();
-
             if (year == -1) {
                 break;
             }
@@ -20,13 +19,16 @@ public class Prompt {
             System.out.println("월을 입력하세요.");
             System.out.print("MONTH> ");
             int month = sc.nextInt();
-
             if (month < 1 || month > 12){
+                System.out.println("잘못된 입력입니다.");
                 continue;
             }
-            else {
-                cal.printCalendar(year, month);
-            }
+
+            System.out.println("첫번째 요일을 입력하세요. (SU, MO, WE, TH, FR, SA)");
+            System.out.print("WEEKDAY> ");
+            String weekday = sc.next();
+
+            cal.printCalendar(year, month, weekday);
         }
         System.out.println("Bye~");
     }
